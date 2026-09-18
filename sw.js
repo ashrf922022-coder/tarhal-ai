9fc1e46ae728df142b4f9d6987c5659c49174be7
ashrf922022-coder <ashrf92.2022@gmail.com>
2026-09-18 15:24:21 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="tarhal-ai-v1"; const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return r}).catch(()=>caches.match("./index.html"))))});
